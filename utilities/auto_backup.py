78277b28a85645427649ea5e65774dd3d92ab674
aDarkDev <121636230+aDarkDev@users.noreply.github.com>
2024-04-25 12:12:43 +0330

    use aiohttp for backups

--- utilities/auto_backup.py
+++ utilities/auto_backup.py
@@ -1,5 +1,5 @@
-import requests
-import time
+import aiohttp
+import asyncio
 import os
 
 from enums.datebaseenums import database
@@ -16,23 +16,25 @@ def restore_backup(name:str):
     command = "mongorestore --gzip --archive={}".format(name)
     os.system(command)
 
-def _send_file_tl(token, chat_id, filePath):
-    url = f"https://api.telegram.org/bot{token}/sendDocument"
+async def _send_file_tl(token, chat_id, filePath):
+
+  url = f"https://api.telegram.org/bot{token}/sendDocument"
+
+  async with aiohttp.ClientSession() as session:
 
     files = {'document': open(filePath, 'rb')}
 
     data = {'chat_id': chat_id}
 
-    response = requests.post(url, files=files, data=data)
+    async with session.post(url, files=files, data=data) as response:
+      return await response.json()
 
-    return response.json()
-
-def backup_process(stop_event):
+async def backup_process(stop_event):
     while not stop_event.is_set():
         backup()
-        _send_file_tl(
+        await _send_file_tl(
             cohandler.config["bot"]["bot_token"],
             cohandler.config["bot"]["backup_channel"],
             "backup.gz"
         )
-        time.sleep(300)
+        await asyncio.sleep(300)
