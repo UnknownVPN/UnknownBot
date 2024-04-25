be0cc482849eab2883f1e38e48e9c5fd50ec3ab9
aDarkDev <121636230+aDarkDev@users.noreply.github.com>
2024-04-25 13:05:01 +0330

    update prices

--- enums/prices.py
+++ enums/prices.py
@@ -1,32 +1,54 @@
 class prices:
+    service_1 = 38_000
+    service_2 = 47_000
+    service_3 = 48_000
+    service_4 = 79_000
+    service_5 = 74_000
+    service_6 = 128_000
+    service_7 = 97_000
+    service_8 = 165_000
+    
     giftAmount = 10_000
-    service_1 = 99_000
-    service_2 = 129_000
-    service_3 = 130_000
-    service_4 = 197_000
-    service_5 = 186_000
-    service_6 = 297_000
-    service_7 = 269_000
-    service_8 = 385_000
     userCount = 10_000
-    data = 3_000
-
+    data = 1_000
+    
     size = {
-        1: {25: service_1, 50: service_2},
-        2: {50: service_3, 100: service_4},
-        3: {75: service_5, 150: service_6},
-        4: {100: service_7, 200: service_8},
-    }
+        1: {
+            25: service_1,
+            50: service_2
+        },
+        2: {
+            50 : service_3,
+            100 : service_4
+        },
+        3: {
+            75: service_5,
+            150: service_6
+        },
+        4: {
+            100: service_7,
+            200: service_8
+        }
+    }  
+
+    cs1 = 98_000
+    cs2 = 146_000
+    cs3 = 154_000
+    cs4 = 263_000
+    cs5 = 218_000
+    cs6 = 382_000
+    cs7 = 281_000
+    cs8 = 499_000
 
     service_dict = {
-        70_000: 99_000,
-        104_000: 129_000,
-        110_000: 130_000,
-        188_000: 197_000,
-        156_000: 186_000,
-        273_000: 297_000,
-        201_000: 269_000,
-        357_000: 385_000,
+        cs1: service_1,
+        cs2: service_2,
+        cs3: service_3,
+        cs4: service_4,
+        cs5: service_5,
+        cs6: service_6,
+        cs7: service_7,
+        cs8: service_8
     }
 
     str_conf = [
