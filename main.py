6b76a313406efcf1f7b7bf546ecdc5cedac301a7
aDarkDev <121636230+aDarkDev@users.noreply.github.com>
2024-04-26 23:44:52 +0330

    add protocol tag

--- main.py
+++ main.py
@@ -296,11 +296,12 @@ async def user_services(client, message):
     if user_services_list:
         for services in user_services_list:
             service = await unknownApi.getApiServiceInfo(services["license"])
+            protocol_name = "ᵛᵐᵉˢˢ" if service["service"]["protocol"] == "vmess" else "ᵛˡᵉˢˢ"
             if service:
                 servicebuttons.append(
                     [
                         InlineKeyboardButton(
-                            f"{services['flag']} {services['name']}",
+                            f"{services['flag']} {services['name']} {protocol_name}",
                             callback_data=f"getservice:{services['license']}",
                         )
                     ]
--- services/card/card_service.py
+++ services/card/card_service.py
@@ -141,7 +141,8 @@ async def get_transaction_status(card: int, amount: int):
         ).json()
 
         if isinstance(data,bool):
-            logger(__name__).info(f"Payment Success! {card} {amount} {data}")
+            if data:
+                logger(__name__).info(f"Payment Success! {card} {amount} {data}")
             return data
         elif isinstance(data,dict):
             if data["status"]:
